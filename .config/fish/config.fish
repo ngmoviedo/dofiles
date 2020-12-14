22973201e89489586cc95fe155a7b8ad5e718001
nicolas <ngmoviedo@gmx.com>
2020-12-14 14:22:25 +0100

    Saved aliases as functions (the fish way)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,19 +8,4 @@ end
 # Alias
 # thefuck
 thefuck --alias | source
-# neovim
-alias vim='nvim'
-#alias vi='nvim' # done via symlink
-# xterm
-#alias xterm='kitty' # done via symlink (to open text files in pcmanfm-qt using
-#nvim)
-# matlab (no GUI)
-alias matlab-cli='set -x MESA_LOADER_DRIVER_OVERRIDE i965; matlab -nodesktop -nosplash'
-# wheather forecast
-alias weather='wttr Oviedo'
-# pager
-alias less='nvimpager -p'
-# cat alternative
-alias cat='bat --paging=never'
-
 
