eeef55f2ec1f6879521efd820a3de35a072c4e53
nicolas <ngmoviedo@gmx.com>
2020-12-15 00:03:09 +0100

    Bind forward-char to ç instead of ñ

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
-	bind -M insert \u00F1 'forward-char'	
-	bind \u00F1 'forward-char' 'forward-char'
+	bind -M insert ç 'forward-char'	
+	bind ç 'forward-char' 'forward-char'
 	bind -M insert \cf 'thefuck-command-line'
 	bind \cf 'thefuck-command-line'
 end
